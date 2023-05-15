8e7c1875ee53b29e64302547469f0c6b7bb4aa68
Yanju Chen <yanju@cs.ucsb.edu>
2023-05-15 01:26:10 -0700

    update rewriter with more cases

--- inkutils/rewriter.py
+++ inkutils/rewriter.py
@@ -2,8 +2,52 @@ import json
 import sys
 import re
 
-def rewrite_invoke2call(arg_str):
-    return arg_str.replace("invoke", "call")
+def rewrite_invoke2call_expr(arg_list):
+    new_list = []
+    i = 0
+    while i<len(arg_list):
+        p = arg_list[i]
+        if p.startswith("invoke ") and i<len(arg_list)-1:
+            q = arg_list[i+1]
+            if q.startswith("to label "):
+                # rewrite!
+                # FIXME: dump the 'unwind' part
+                new_q = q.split(" ")[:3]
+                new_q[0] = "br" # replace "to" to "br"
+                new_list.append(p.replace("invoke", "call"))
+                new_list.append(" ".join(new_q))
+                i += 2
+            else:
+                new_list.append(arg_list[i])
+                i += 1
+        else:
+            new_list.append(arg_list[i])
+            i += 1
+    return new_list
+
+def rewrite_invoke2call_assign(arg_list):
+    new_list = []
+    i = 0
+    while i<len(arg_list):
+        p = arg_list[i]
+        plist = p.split(" ")
+        if " ".join(plist[1:3])=="= invoke" and i<len(arg_list)-1:
+            q = arg_list[i+1]
+            if q.startswith("to label "):
+                # rewrite!
+                # FIXME: dump the 'unwind' part
+                new_q = q.split(" ")[:3]
+                new_q[0] = "br" # replace "to" to "br"
+                new_list.append(p.replace("invoke", "call"))
+                new_list.append(" ".join(new_q))
+                i += 2
+            else:
+                new_list.append(arg_list[i])
+                i += 1
+        else:
+            new_list.append(arg_list[i])
+            i += 1
+    return new_list          
     
 def rewrite(*args):
     for p in args:
@@ -15,7 +59,11 @@ def rewrite(*args):
                 g.write(raw_str)
         
         # rewrite
-        new_str = rewrite_invoke2call(raw_str)
+        raw_list = raw_str.split("\n")
+        raw_list = [p.strip() for p in raw_list] # remove leading whitespaces
+        new_list = rewrite_invoke2call_expr(raw_list)
+        new_list = rewrite_invoke2call_assign(new_list)
+        new_str = "\n".join(new_list)
 
         # write back
         with open(p, "w") as f:
